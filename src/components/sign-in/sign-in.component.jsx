ba680c773ca8d189ac3067ba4a7f7abf4bd3753e
Arthur Sakemi <arthursakemi@gmail.com>
2020-03-02 13:10:34 -0300

    added redux, implemented userReducer and userActions to Headder component

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -41,7 +41,7 @@ class SignIn extends React.Component {
   render() {
     return (
       <div className="sign-in">
-        <h2>I already have and account</h2>
+        <h2 className="title">I already have and account</h2>
         <span>Sign in with your email and password</span>
 
         <form onSubmit={this.handleSubmit}>
